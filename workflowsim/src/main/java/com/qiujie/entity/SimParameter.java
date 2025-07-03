692fe96cf117dba4c2b9b15c21ece20fd260bd1b
qiujie <hurtlife2020@gmail.com>
2025-07-03 18:40:33 +0800

    Remove experimentName field

--- workflowsim/src/main/java/com/qiujie/entity/SimParameter.java
+++ workflowsim/src/main/java/com/qiujie/entity/SimParameter.java
@@ -9,34 +9,31 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class SimParameter {
-    private String name;
-    private String experimentName;
     private long seed;
     private List<String> daxList;
     private String plannerClass;
     private Parameter parameter;
 
-    public SimParameter(String experimentName, long seed, List<String> daxList, Class<? extends WorkflowPlannerAbstract> plannerClass, Parameter parameter) {
-        this.experimentName = experimentName;
+    public SimParameter(long seed, List<String> daxList, Class<? extends WorkflowPlannerAbstract> plannerClass, Parameter parameter) {
         this.seed = seed;
         this.daxList = daxList;
         this.plannerClass = plannerClass.getName();
         this.parameter = parameter;
     }
 
-    public SimParameter(String experimentName, long seed, List<String> daxList, Class<? extends WorkflowPlannerAbstract> plannerClass) {
-        this(experimentName, seed, daxList, plannerClass, new Parameter());
+    public SimParameter(long seed, List<String> daxList, Class<? extends WorkflowPlannerAbstract> plannerClass) {
+        this(seed, daxList, plannerClass, new Parameter());
     }
 
 
     /**
      * single workflow
      */
-    public SimParameter(String experimentName, long seed, String daxPath, Class<? extends WorkflowPlannerAbstract> plannerClass, Parameter parameter) {
-        this(experimentName, seed, List.of(daxPath), plannerClass, parameter);
+    public SimParameter(long seed, String daxPath, Class<? extends WorkflowPlannerAbstract> plannerClass, Parameter parameter) {
+        this(seed, List.of(daxPath), plannerClass, parameter);
     }
 
-    public SimParameter(String experimentName, long seed, String daxPath, Class<? extends WorkflowPlannerAbstract> plannerClass) {
-        this(experimentName, seed, List.of(daxPath), plannerClass, new Parameter());
+    public SimParameter(long seed, String daxPath, Class<? extends WorkflowPlannerAbstract> plannerClass) {
+        this(seed, List.of(daxPath), plannerClass, new Parameter());
     }
 }
