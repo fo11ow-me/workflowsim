ad0f8532cbcf18f4b441c0c5d10a8839f921aeb6
qiujie <hurtlife2020@gmail.com>
2025-07-16 15:11:14 +0800

    Update

--- workflowsim/src/main/java/com/qiujie/example/Example04.java
+++ workflowsim/src/main/java/com/qiujie/example/Example04.java
@@ -3,6 +3,7 @@ package com.qiujie.example;
 
 import ch.qos.logback.classic.Level;
 import com.qiujie.comparator.DepthComparator;
+import com.qiujie.comparator.JobNumComparator;
 import com.qiujie.comparator.LengthComparator;
 import com.qiujie.aop.ClockModifier;
 import com.qiujie.entity.Parameter;
@@ -33,13 +34,14 @@ public class Example04 {
         org.cloudbus.cloudsim.Log.disable();
         Log.setLevel(Level.DEBUG);
         List<String> daxList = List.of(
-                "Genome_50");
+                "Genome_50",
+                "Montage_100");
         VMS = 50;
         SimStarter simStarter = new SimStarter(new SimParameter(seed, daxList, RandomPlanner.class, new Parameter().setWorkflowComparator(DepthComparator.class).setAscending(true)));
 //        Log.setLevel(Level.OFF);
         SimStarter simStarter1 = new SimStarter(new SimParameter(seed, daxList, RandomPlanner.class, new Parameter().setWorkflowComparator(LengthComparator.class).setAscending(true)));
-        SimStarter simStarter2 = new SimStarter(new SimParameter(seed, daxList, RandomPlanner.class, new Parameter().setWorkflowComparator(LengthComparator.class).setAscending(false)));
-        SimStarter simStarter3 = new SimStarter(new SimParameter(seed, daxList, RandomPlanner.class, new Parameter().setWorkflowComparator(LengthComparator.class).setAscending(true)));
+        SimStarter simStarter2 = new SimStarter(new SimParameter(seed, daxList, HEFTPlanner.class, new Parameter().setWorkflowComparator(LengthComparator.class).setAscending(false)));
+        SimStarter simStarter3 = new SimStarter(new SimParameter(seed, daxList, HEFTPlanner.class, new Parameter().setWorkflowComparator(JobNumComparator.class).setAscending(false)));
         List<SimStarter> simStarterList = List.of(simStarter, simStarter1, simStarter2, simStarter3);
         ExperimentUtil.printExperimentResult(simStarterList.stream().map(SimStarter::getResult).toList());
         System.out.println(name + " run " + (System.currentTimeMillis() - seed) / 1000.0 + "s");
