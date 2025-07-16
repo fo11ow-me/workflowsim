67c600ccca92f730e0367cff7a6e5a29beea25f1
qiujie <hurtlife2020@gmail.com>
2025-07-16 09:54:47 +0800

    Generate different workflows

--- workflowsim/src/main/java/com/qiujie/util/WorkflowGenerator.java
+++ workflowsim/src/main/java/com/qiujie/util/WorkflowGenerator.java
@@ -4,8 +4,6 @@ import generator.RunAll;
 import generator.app.*;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
 import static com.qiujie.Constants.*;
 
@@ -14,10 +12,6 @@ public class WorkflowGenerator {
 
 
     public static void main(String[] args) throws Exception {
-
-        Map<Class<? extends Application>, Integer> map = new HashMap<>();
-        map.put(Montage.class, 100000);
-        map.put(Genome.class, 100);
 
         File dir = new File(DAX_DIR);
         if (!dir.exists()) {
@@ -28,7 +22,7 @@ public class WorkflowGenerator {
             for (Integer jobNum : JOB_NUM_LIST) {
                 for (int i = 0; i < INSTANCE_NUM; i++) {
                     Application app = clazz.getDeclaredConstructor().newInstance();
-                    RunAll.run(app, new File(dir.getAbsolutePath() + "\\" + app.getClass().getSimpleName() + "_" + jobNum + "_" + i + ".xml"), "-n", jobNum + "", "-f", map.get(clazz) + "");
+                    RunAll.run(app, new File(dir.getAbsolutePath() + "\\" + app.getClass().getSimpleName() + "_" + jobNum + "_" + i + ".xml"), "-n", jobNum + "");
                 }
             }
         }
@@ -37,7 +31,7 @@ public class WorkflowGenerator {
         for (Class<? extends Application> clazz : APP_LIST) {
             for (Integer jobNum : JOB_NUM_LIST) {
                 Application app = clazz.getDeclaredConstructor().newInstance();
-                RunAll.run(app, new File(dir.getAbsolutePath() + "\\" + app.getClass().getSimpleName() + "_" + jobNum + ".xml"), "-n", jobNum + "", "-f", map.get(clazz) + "");
+                RunAll.run(app, new File(dir.getAbsolutePath() + "\\" + app.getClass().getSimpleName() + "_" + jobNum + ".xml"), "-n", jobNum + "", "-f");
             }
         }
     }
