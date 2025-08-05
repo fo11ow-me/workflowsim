ebf2a1783ee67d3f507cb1fcd5090478414c82ae
qiujie <hurtlife2020@gmail.com>
2025-08-05 16:58:52 +0800

    Add field

--- workflowsim/src/main/java/com/qiujie/entity/ExecWindow.java
+++ workflowsim/src/main/java/com/qiujie/entity/ExecWindow.java
@@ -11,15 +11,17 @@ public class ExecWindow {
     private static final AtomicInteger nextId = new AtomicInteger(0);
 
     private final int id;
-
-    // execution start time
+    private final int insertPos;
     private final double startTime;
-    // execution finish time
-    private final double finishTime;
+    private double finishTime;
+    private Fv fv;
+    private double elecCost;
 
-    public ExecWindow(double startTime, double finishTime) {
+    public ExecWindow(double startTime, double finishTime, int insertPos, Fv fv) {
         this.id = nextId.getAndIncrement();
         this.startTime = startTime;
         this.finishTime = finishTime;
+        this.insertPos = insertPos;
+        this.fv = fv;
     }
 }
