58a4834e6eef5c68f2e92869e039b27f7c994e25
qiujie <hurtlife2020@gmail.com>
2025-08-12 20:14:44 +0800

    Add constructor

--- workflowsim/src/main/java/com/qiujie/entity/Result.java
+++ workflowsim/src/main/java/com/qiujie/entity/Result.java
@@ -1,9 +1,13 @@
 package com.qiujie.entity;
 
+import com.qiujie.core.WorkflowBroker;
+import com.qiujie.planner.WorkflowPlannerAbstract;
+import com.qiujie.util.ExperimentUtil;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -27,6 +31,36 @@ public class Result implements Serializable {
     private int overdueCount;
     private double plnRuntime;
     private double runtime;
+
+    public static final Result POISON_PILL = new Result().setId(-1);
+
+
+    public Result() {
+        this.daxList = new ArrayList<>();
+    }
+
+    public Result(SimParam simParam, WorkflowPlannerAbstract planner, WorkflowBroker broker, double runtime) {
+        this();
+        int finishedCloudlets = broker.getCloudletReceivedList().size();
+        int totalCloudlets = broker.getWorkflowList().stream().mapToInt(Workflow::getJobNum).sum();
+        setId(simParam.getId());
+        setName(planner.toString());
+        setWorkflowComparator(ExperimentUtil.getPrefixFromClassName(simParam.getParam().getWorkflowComparator()));
+        setAscending(simParam.getParam().isAscending());
+        setDeadlineFactor(simParam.getParam().getDeadlineFactor());
+        setReliabilityFactor(simParam.getParam().getReliabilityFactor());
+        setJobSequenceStrategy(simParam.getParam().getJobSequenceStrategy().name());
+        setNeighborhoodFactor(simParam.getParam().getNeighborhoodFactor());
+        setSlackTimeFactor(simParam.getParam().getSlackTimeFactor());
+        getDaxList().addAll(simParam.getDaxList());
+        setCompletionDetail(String.format("%.2f%% (%d / %d)", finishedCloudlets * 100.0 / totalCloudlets, finishedCloudlets, totalCloudlets));
+        setElecCost(broker.getCloudletReceivedList().stream().mapToDouble(cloudlet -> ((Job) cloudlet).getElecCost()).sum());
+        setFinishTime(broker.getCloudletReceivedList().getLast().getExecFinishTime());
+        setRetryCount(broker.getCloudletReceivedList().stream().mapToInt(cloudlet -> ((Job) cloudlet).getRetryCount()).sum());
+        setOverdueCount((int) broker.getWorkflowList().stream().filter(Workflow::isOverdue).count());
+        setPlnRuntime(planner.getRuntime());
+        setRuntime(runtime);
+    }
 
 
     @Override
