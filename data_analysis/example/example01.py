7cbf03f9cf8b858f9eb181581e85701fe90027b6
qiujie <hurtlife2020@gmail.com>
2025-08-18 17:14:29 +0800

    Add algorithm comparison example

--- data_analysis/example/example01.py
+++ data_analysis/example/example01.py
@@ -1,20 +1,26 @@
 import unittest
-from src.util import anova
+from src.util import anova, plot_comparison_chart
 
 
 class Main(unittest.TestCase):
     def setUp(self):
-        self.base_path = "..\\..\\data\\result\\"
-        self.output_dir = "..\\..\\data\\anova"
+        self.result_dir = "..\\..\\data\\result\\"
+        self.anova_dir = "..\\..\\data\\anova"
+        self.comparison_dir = "..\\..\\data\\comparison"
+
 
     def test_two_way_anova(self):
         """Test two-way ANOVA with workflowComparator and ascending"""
         anova(
-            json_path=self.base_path + "Example06.jsonl",
+            path=self.result_dir + "Example06.csv",
             target_variable="elecCost",
             group_variables=["workflowComparator", "ascending"],
-            output_dir=self.output_dir
+            output_dir=self.anova_dir
         )
+
+    def test_comparison(self):
+        plot_comparison_chart(self.result_dir + "Example07.csv", x_axis="workflowComparator",
+                              output_dir=self.comparison_dir)
 
 
 if __name__ == '__main__':
