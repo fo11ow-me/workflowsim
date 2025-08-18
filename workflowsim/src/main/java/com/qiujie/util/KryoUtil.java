b97f8b910f69678ca9b134786f4046652a4aeee7
qiujie <hurtlife2020@gmail.com>
2025-08-18 16:57:11 +0800

    Register class

--- workflowsim/src/main/java/com/qiujie/util/KryoUtil.java
+++ workflowsim/src/main/java/com/qiujie/util/KryoUtil.java
@@ -1,13 +1,17 @@
 package com.qiujie.util;
 
 import com.esotericsoftware.kryo.kryo5.Kryo;
+import com.qiujie.Serializer.ClassSerializer;
 import com.qiujie.entity.*;
+import com.qiujie.enums.JobSequenceStrategyEnum;
+
 import java.util.*;
 
 public class KryoUtil {
 
     private static final ThreadLocal<Kryo> kryoThreadLocal = ThreadLocal.withInitial(() -> {
         Kryo kryo = new Kryo();
+        kryo.setRegistrationRequired(true);
         kryo.register(String.class);
         kryo.register(Dax.class);
         kryo.register(Dax.Job.class);
@@ -16,7 +20,11 @@ public class KryoUtil {
         kryo.register(Freq2Power.class);
         kryo.register(ArrayList.class);
         kryo.register(HashMap.class);
-        kryo.register(HashSet.class);
+        kryo.register(SimParam.class);
+        kryo.register(Param.class);
+        kryo.register(JobSequenceStrategyEnum.class);
+        kryo.register(Result.class);
+        kryo.register(Class.class, new ClassSerializer());
         try {
             kryo.register(Class.forName("java.util.ImmutableCollections$ListN"));
         } catch (ClassNotFoundException e) {
