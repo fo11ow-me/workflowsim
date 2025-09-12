ddd9ec146f0d689718dfbc20e2151c680eb067a4
qiujie <hurtlife2020@gmail.com>
2025-09-12 16:22:46 +0800

    Add equals and hashcode method

--- workflowsim/src/main/java/com/qiujie/entity/SimParam.java
+++ workflowsim/src/main/java/com/qiujie/entity/SimParam.java
@@ -1,15 +1,19 @@
 package com.qiujie.entity;
 
 import com.qiujie.planner.WorkflowPlannerAbstract;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Data
 @Accessors(chain = true)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SimParam {
 
     private static final AtomicInteger nextId = new AtomicInteger(0);
@@ -21,15 +25,10 @@ public class SimParam {
 
     public static final SimParam POISON_PILL = new SimParam().setId(-1);
 
-    public SimParam() {
-        this.id = nextId.getAndIncrement();
-        this.daxList = new ArrayList<>();
-    }
-
     public SimParam(long seed, List<String> daxList, Class<? extends WorkflowPlannerAbstract> plannerClass, Param param) {
-        this();
+        this.id = nextId.getAndIncrement();
         this.seed = seed;
-        this.daxList.addAll(daxList);
+        this.daxList = new ArrayList<>(daxList);
         this.plannerClass = plannerClass;
         this.param = param;
     }
@@ -43,14 +42,23 @@ public class SimParam {
      * single workflow
      */
     public SimParam(long seed, String dax, Class<? extends WorkflowPlannerAbstract> plannerClass, Param param) {
-        this();
-        this.seed = seed;
-        this.daxList.add(dax);
-        this.plannerClass = plannerClass;
-        this.param = param;
+        this(seed, List.of(dax), plannerClass, param);
     }
 
     public SimParam(long seed, String dax, Class<? extends WorkflowPlannerAbstract> plannerClass) {
         this(seed, dax, plannerClass, new Param());
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        SimParam simParam = (SimParam) object;
+        return id == simParam.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
